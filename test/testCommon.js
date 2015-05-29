0173a5a249a21e94ee1e3bdd00338cdb8c1022ac
Luc Patiny <luc@patiny.com>
2015-05-29 15:17:33 +0200

    Add test case to prevent the previous bug

--- test/testCommon.js
+++ test/testCommon.js
@@ -49,3 +49,24 @@ describe('We check common array similarity 3 (widthTop=0)', function () {
         result.similarity.should.approximately(0.75,0.001);
     });
 });
+
+describe('We check common array similarity 4 (check the setFromTo)', function () {
+    var comparator=new Comparator({common: true, widthBottom: 0.2, widthTop: 0});
+    comparator.setPeaks1([[1,1],[2.05,1],[3,1]]);
+    comparator.setPeaks2([[1,1],[2,1],[4,1]]);
+    comparator.setFromTo(0.5, 1.5);
+    var result=comparator.getSimilarity();
+    it('getExtract1', function () {
+        result.extract1.should.eql([[1,1]]);
+    });
+    it('getExtract2', function () {
+        result.extract2.should.eql([[1,1]]);
+    });
+    it('similarity', function () {
+        result.similarity.should.eql(1);
+    });
+});
+
+
+
+
