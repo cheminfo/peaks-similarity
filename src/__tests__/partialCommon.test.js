960fd24763c34c83ab50e2629b8994f1d51f3aac
Luc Patiny <luc@patiny.com>
2022-11-23 13:32:35 +0100

    chore: refactor test partialCommon

--- src/__tests__/partialCommon.test.js
+++ src/__tests__/partialCommon.test.js
@@ -1,8 +1,8 @@
-import { describe, it, expect } from 'vitest';
+import { test, expect } from 'vitest';
 
 import { Comparator } from '..';
 
-describe('We check common array similarity 1', () => {
+test('We check common array similarity 1', () => {
   const comparator = new Comparator({
     common: true,
     widthBottom: 0.2,
@@ -17,18 +17,12 @@ describe('We check common array similarity 1', () => {
     [1, 1],
   ]);
   const result = comparator.getSimilarity();
-  it('getExtract1', () => {
-    expect(result.extract1).toStrictEqual([[1], [1]]);
-  });
-  it('getExtract2', () => {
-    expect(result.extract2).toStrictEqual([[1], [1]]);
-  });
-  it('similarity', () => {
-    expect(result.similarity).toBe(1);
-  });
+  expect(result.extract1).toStrictEqual([[1], [1]]);
+  expect(result.extract2).toStrictEqual([[1], [1]]);
+  expect(result.similarity).toBe(1);
 });
 
-describe('We check common array similarity 2', () => {
+test('We check common array similarity 2', () => {
   const comparator = new Comparator({
     common: true,
     widthBottom: 0.2,
@@ -45,24 +39,18 @@ describe('We check common array similarity 2', () => {
     [4, 1],
   ]);
   const result = comparator.getSimilarity();
-  it('getExtract1', () => {
-    expect(result.extract1).toStrictEqual([
-      [1, 2.05],
-      [0.5, 0.5],
-    ]);
-  });
-  it('getExtract2', () => {
-    expect(result.extract2).toStrictEqual([
-      [1, 2],
-      [0.5, 0.5],
-    ]);
-  });
-  it('similarity', () => {
-    expect(result.similarity).toBe(1);
-  });
+  expect(result.extract1).toStrictEqual([
+    [1, 2.05],
+    [0.5, 0.5],
+  ]);
+  expect(result.extract2).toStrictEqual([
+    [1, 2],
+    [0.5, 0.5],
+  ]);
+  expect(result.similarity).toBe(1);
 });
 
-describe('We check common array similarity 3 (widthTop=0)', () => {
+test('We check common array similarity 3 (widthTop=0)', () => {
   const comparator = new Comparator({
     common: true,
     widthBottom: 0.2,
@@ -79,24 +67,18 @@ describe('We check common array similarity 3 (widthTop=0)', () => {
     [4, 1],
   ]);
   const result = comparator.getSimilarity();
-  it('getExtract1', () => {
-    expect(result.extract1).toStrictEqual([
-      [1, 2.05],
-      [0.5, 0.5],
-    ]);
-  });
-  it('getExtract2', () => {
-    expect(result.extract2).toStrictEqual([
-      [1, 2],
-      [0.5, 0.5],
-    ]);
-  });
-  it('similarity', () => {
-    expect(result.similarity).toBeCloseTo(0.75, 3);
-  });
+  expect(result.extract1).toStrictEqual([
+    [1, 2.05],
+    [0.5, 0.5],
+  ]);
+  expect(result.extract2).toStrictEqual([
+    [1, 2],
+    [0.5, 0.5],
+  ]);
+  expect(result.similarity).toBeCloseTo(0.75, 3);
 });
 
-describe('We check common array similarity 4 (check the setFromTo)', () => {
+test('We check common array similarity 4 (check the setFromTo)', () => {
   const comparator = new Comparator({
     common: true,
     widthBottom: 0.2,
@@ -114,18 +96,12 @@ describe('We check common array similarity 4 (check the setFromTo)', () => {
   ]);
   comparator.setFromTo(0.5, 1.5);
   const result = comparator.getSimilarity();
-  it('getExtract1', () => {
-    expect(result.extract1).toStrictEqual([[1], [1]]);
-  });
-  it('getExtract2', () => {
-    expect(result.extract2).toStrictEqual([[1], [1]]);
-  });
-  it('similarity', () => {
-    expect(result.similarity).toBe(1);
-  });
+  expect(result.extract1).toStrictEqual([[1], [1]]);
+  expect(result.extract2).toStrictEqual([[1], [1]]);
+  expect(result.similarity).toBe(1);
 });
 
-describe('We check common array with nothing common based on first', () => {
+test('We check common array with nothing common based on first', () => {
   const comparator = new Comparator({
     common: 'first',
     widthBottom: 0.2,
@@ -145,21 +121,15 @@ describe('We check common array with nothing common based on first', () => {
   ]);
 
   const result = comparator.getSimilarity();
-  it('getExtract1', () => {
-    expect(result.extract1).toStrictEqual([
-      [1, 2, 3, 4],
-      [0.25, 0.25, 0.25, 0.25],
-    ]);
-  });
-  it('getExtract2', () => {
-    expect(result.extract2).toStrictEqual([[], []]);
-  });
-  it('similarity', () => {
-    expect(result.similarity).toBe(0);
-  });
+  expect(result.extract1).toStrictEqual([
+    [1, 2, 3, 4],
+    [0.25, 0.25, 0.25, 0.25],
+  ]);
+  expect(result.extract2).toStrictEqual([[], []]);
+  expect(result.similarity).toBe(0);
 });
 
-describe('We check common array with nothing common based on second', () => {
+test('We check common array with nothing common based on second', () => {
   const comparator = new Comparator({
     common: 'second',
     widthBottom: 0.2,
@@ -179,21 +149,15 @@ describe('We check common array with nothing common based on second', () => {
   ]);
 
   const result = comparator.getSimilarity();
-  it('getExtract1', () => {
-    expect(result.extract1).toStrictEqual([[], []]);
-  });
-  it('getExtract2', () => {
-    expect(result.extract2).toStrictEqual([
-      [5, 6, 7, 8],
-      [0.25, 0.25, 0.25, 0.25],
-    ]);
-  });
-  it('similarity', () => {
-    expect(result.similarity).toBe(0);
-  });
+  expect(result.extract1).toStrictEqual([[], []]);
+  expect(result.extract2).toStrictEqual([
+    [5, 6, 7, 8],
+    [0.25, 0.25, 0.25, 0.25],
+  ]);
+  expect(result.similarity).toBe(0);
 });
 
-describe('We check common array with nothing common based on both', () => {
+test('We check common array with nothing common based on both', () => {
   const comparator = new Comparator({
     common: 'both',
     widthBottom: 0.2,
@@ -213,18 +177,12 @@ describe('We check common array with nothing common based on both', () => {
   ]);
 
   const result = comparator.getSimilarity();
-  it('getExtract1', () => {
-    expect(result.extract1).toStrictEqual([[], []]);
-  });
-  it('getExtract2', () => {
-    expect(result.extract2).toStrictEqual([[], []]);
-  });
-  it('similarity', () => {
-    expect(result.similarity).toBe(0);
-  });
+  expect(result.extract1).toStrictEqual([[], []]);
+  expect(result.extract2).toStrictEqual([[], []]);
+  expect(result.similarity).toBe(0);
 });
 
-describe('We check common array with nothing common based on all', () => {
+test('We check common array with nothing common based on all', () => {
   const comparator = new Comparator({
     common: 'all',
     widthBottom: 0.2,
@@ -244,19 +202,13 @@ describe('We check common array with nothing common based on all', () => {
   ]);
 
   const result = comparator.getSimilarity();
-  it('getExtract1', () => {
-    expect(result.extract1).toStrictEqual([
-      [1, 2, 3, 4],
-      [0.25, 0.25, 0.25, 0.25],
-    ]);
-  });
-  it('getExtract2', () => {
-    expect(result.extract2).toStrictEqual([
-      [5, 6, 7, 8],
-      [0.25, 0.25, 0.25, 0.25],
-    ]);
-  });
-  it('similarity', () => {
-    expect(result.similarity).toBe(0);
-  });
+  expect(result.extract1).toStrictEqual([
+    [1, 2, 3, 4],
+    [0.25, 0.25, 0.25, 0.25],
+  ]);
+  expect(result.extract2).toStrictEqual([
+    [5, 6, 7, 8],
+    [0.25, 0.25, 0.25, 0.25],
+  ]);
+  expect(result.similarity).toBe(0);
 });
